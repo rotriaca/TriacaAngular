889af9e1927273856b1098204171ebf58c756d82
Rocio Triaca <rocio.triaca@gmail.com>
2022-07-21 15:27:56 -0300

    Primer entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { DestinosComponent } from './destinos/destinos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    DestinosComponent
   ],
   imports: [
     BrowserModule
